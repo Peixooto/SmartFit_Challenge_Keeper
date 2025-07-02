6db9e03b5e15496f65816e1387f9f3023f41a4ab
Lucas Peixoto <lucas.peixoto@indt.org.br>
2025-07-01 23:48:13 -0400

    wip: 1:39 min build forms component integration

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -3,7 +3,24 @@ import { Component, OnInit } from '@angular/core';
 import {FormBuilder,FormGroup,ReactiveFormsModule} from '@angular/forms';
 import { GetUnitsService } from '../../services/get-units.service';
 import { HttpClientModule } from '@angular/common/http';
+import { Location } from '../../types/location.interface';
 
+const OPENING_HOURS={
+  morning:{
+    first:'06',
+    last:'12'
+  },
+   afternoon:{
+    first:'12',
+    last:'18'
+  },
+   night:{
+    first:'18',
+    last:'23'
+   }
+}
+
+type HOUR_INDEXES = 'morning'|'afternoon' | 'night'
 @Component({
   selector: 'app-forms',
   standalone: true,
@@ -12,22 +29,59 @@ import { HttpClientModule } from '@angular/common/http';
   styleUrl: './forms.component.scss'
 })
 export class FormsComponent implements OnInit{
-  results = [];
+  results: Location[]= [];
+  filteredResults: Location[] = [];
+
   formGroup!: FormGroup;
   
   constructor(private formBuilder: FormBuilder, private unitService: GetUnitsService){}
 
 
   ngOnInit(): void {
-    this.unitService.getAllUnits().subscribe(data => console.log(data))
+    this.unitService.getAllUnits().subscribe(data => {
+      this.results = data.locations;
+      this.filteredResults = data.locations
+    })
     this.formGroup = this.formBuilder.group({
       hour:'',
-      showClosed: false
+      showClosed: true
     })
   }
 
+  transformWeekday(weekday:number){
+    switch (weekday){
+      case 0:
+        return 'Dom.'
+    
+    case 6:
+      return 'Sáb.'
+
+    default:
+      return 'Seg. à Sex.'
+    }
+  }
+
+  filterUnits(unit:Location,opened:boolean,open_hour:string,close_hour:string){
+    let open_hour_filter = parseInt(open_hour,10)
+    let close_hour_filter = parseInt(close_hour,10)
+
+    let todays_weekday =  this.transformWeekday (new Date().getDay());
+
+    for(let i = 0; i< unit.schedules.length; i++){
+      let schedules_hour = unit.schedules[i].hour
+      let schedules_weekday = unit.schedules[i].weekdays
+    }    
+  }
+
   onSubmit():void {
-    console.log(this.formGroup.value)
+    const OPEN_HOUR = OPENING_HOURS[this.formGroup.value.hour as HOUR_INDEXES].first
+    const CLOSE_HOUR = OPENING_HOURS[this.formGroup.value.hour as HOUR_INDEXES].last
+
+   if (!this.formGroup.value.showClosed){
+    this.filteredResults = this.results.filter(location => location.opened === true)
+   } else {
+    this.filteredResults = this.results
+   }
   }
 
   onClean():void {
--- src/app/types/units-response.interface.ts
+++ src/app/types/units-response.interface.ts
@@ -1,4 +1,4 @@
-import { Location } from "./location.interface"
+import {Location } from "./location.interface"
 
 export interface UnitsResponse{
     current_country_id: number,
